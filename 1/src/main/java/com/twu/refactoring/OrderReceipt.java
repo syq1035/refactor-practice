80b6e7cc467ca1adf1073c54358c7dcb938b8f7c
syq <1035394794@qq.com>
2020-09-15 00:07:51 +0800

    [syq] refactor practice 1

--- 1/src/main/java/com/twu/refactoring/OrderReceipt.java
+++ 1/src/main/java/com/twu/refactoring/OrderReceipt.java
@@ -8,34 +8,31 @@ package com.twu.refactoring;
  * 
  */
 public class OrderReceipt {
-    private Order o;
+    private Order order;
 
-    public OrderReceipt(Order o) {
-        this.o = o;
+    public OrderReceipt(Order order) {
+        this.order = order;
 	}
 
 	public String printReceipt() {
 		StringBuilder output = new StringBuilder();
 
-		// print headers
-		output.append("======Printing Orders======\n");
+		final String HEADER = "======Printing Orders======\n";
+		output.append(HEADER);
 
 		// print date, bill no, customer name
 //        output.append("Date - " + order.getDate();
-        output.append(o.getCustomerName());
-        output.append(o.getCustomerAddress());
+        output.append(order.getCustomerName());
+        output.append(order.getCustomerAddress());
 //        output.append(order.getCustomerLoyaltyNumber());
 
 		// prints lineItems
 		double totSalesTx = 0d;
 		double tot = 0d;
-		for (LineItem lineItem : o.getLineItems()) {
-			output.append(lineItem.getDescription());
-			output.append('\t');
-			output.append(lineItem.getPrice());
-			output.append('\t');
-			output.append(lineItem.getQuantity());
-			output.append('\t');
+		for (LineItem lineItem : order.getLineItems()) {
+			format(output, lineItem.getDescription());
+			format(output, String.valueOf(lineItem.getPrice()));
+			format(output, String.valueOf(lineItem.getQuantity()));
 			output.append(lineItem.totalAmount());
 			output.append('\n');
 
@@ -53,5 +50,10 @@ public class OrderReceipt {
         // print total amount
 		output.append("Total Amount").append('\t').append(tot);
 		return output.toString();
+	}
+
+	private void format(StringBuilder output, String str) {
+		output.append(str);
+		output.append('\t');
 	}
 }
